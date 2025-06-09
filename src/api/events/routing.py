f1640c5bc81efba2d9a6495c7e7adc013b9a3def
apeksha mehta <>
2025-06-09 16:05:02 +0530

    changed schema for method put and post

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,48 +1,49 @@
 from fastapi import APIRouter
-from .schema import EventSchema,EventListSchema
+from .schema import EventSchema,EventListSchema,EventCreateSchema,EventUpdateSchema
 router = APIRouter()
 
-# GET DATA HERE
-# LIST VIEW
-# GET /api/events/
+
+
+#GET /api/events/
 @router.get("/")
 async def read_events() -> EventListSchema:
     # a bunch of items in a table
     return {
-        "itmes" : [{"id" : 1}, {"id" : 2}, {"id" : 3}],
+        "results" : [{"id":1},{"id":2},{"id":3}],
         "count" : 3
     }
     
     
-# SEND DATA HERE
-# CREATE VIEW
-# POST /api/events/
+#POST /api/events/
 @router.post("/")
-async def create_events(data:dict = {}):
-    # a bunch of items in a table
-    print(data)
-    return data
+async def create_event(payload: EventCreateSchema) -> EventSchema:
+    # a single row
+    print(payload)
+    data = payload.model_dump()
+    return {
+        "id" : 123,
+        **data
+    }
 
-# GET /api/events/12
 @router.get("/{event_id}")
 async def get_event(event_id: str) -> EventSchema:
     # a single row
     return {
         "id" : event_id
     }
-    
 
-# Update this data
-# PUT /api/events/12
 @router.put("/{event_id}")
-async def update_event(event_id: str,payload:dict = {}) -> EventSchema:
+async def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
     # a single row
+    print(event_id)
+    print("--------------------------------next is payload--------------------------------")
+    print(payload)
+    data = payload.model_dump()
     return {
-        "id" : event_id
+        "id" : event_id,   **data
     }
-    
-#     # GET/api/events/12
-# @router.delete("/{event_id}")
+
+# @router.delete("/{event_id}", response_model=EventSchema)
 # async def delete_event(event_id: str) -> EventSchema:
 #     # a single row
 #     return {
--- src/api/events/schema.py
+++ src/api/events/schema.py
@@ -1,13 +1,26 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List,Optional
+from pydantic import BaseModel,Field
 
-class EventCreateSchema(BaseModel):
-    id: int
+
+"""
+id 
+path
+description
+"""
+
+class EventCreateSchema(BaseModel):    
+    page: str
+    description:Optional[str] = Field(default="my description")
+
+class EventUpdateSchema(BaseModel):
+    description: str
     
 
 
 class EventSchema(BaseModel):
     id: int
+    page:Optional[str] = ""
+    description:Optional[str] = ""
     
 
 class EventListSchema(BaseModel):
